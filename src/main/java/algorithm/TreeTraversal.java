68db28923f34e8815d58500e6c0544d70eb5eb49
xuan <xuan.zhang@baozun.com>
2019-07-05 18:40:59 +0800

    commit

--- src/main/java/algorithm/TreeTraversal.java
+++ src/main/java/algorithm/TreeTraversal.java
@@ -2,54 +2,74 @@ package algorithm;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * 树的左右序遍历
+ * 树的左中右序遍历
+ *
+ * 左中右序的判断标准是中间节点什么时候被遍历
  * created by XUAN on 2019/05/06
  */
 public class TreeTraversal {
 
 
+    /**
+     * 前序遍历
+     *
+     * 中--> 左 --> 右
+     */
     @Test
-    public void binaryTreeTraversal () {
-        TreeNode root = getTree(20);
-        leftTraversal(root);
+    public void frontBinaryTreeTraversal () {
+        TreeNode root = BuildTree.getTree(10);
+        frontTraversal(root);
     }
 
-    private void leftTraversal(TreeNode node) {
+    private void frontTraversal(TreeNode node) {
         System.out.println(node.getData());
-        // 两if换个位置就是左右序遍历
-        if (node.getRight() != null) {
-            leftTraversal(node.getRight());
-        }
         if (node.getLeft() != null) {
-            leftTraversal(node.getLeft());
+            frontTraversal(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            frontTraversal(node.getRight());
         }
     }
 
-    private static TreeNode getTree (Integer num) {
-        // 创建根节点
-        TreeNode root = new TreeNode();
-        root.setData("0");
-        List<TreeNode> nodes = new ArrayList<TreeNode>();
-        nodes.add(root);
-        for (int i = 1; i <= num; i++) {
-            TreeNode node = new TreeNode();
-            node.setData(String.valueOf(i));
-            // 获得父节点
-            TreeNode parentNode = nodes.get(((i + 1) >> 1) - 1);
-            // 偶数为右节点,奇数为左节点
-            if (i % 2 == 0) {
-                parentNode.setRight(node);
-            } else {
-                parentNode.setLeft(node);
-            }
-            // 将节点放入list方便获得父节点
-            nodes.add(node);
+    /**
+     * 中序遍历
+     * 左子树-->中间节点-->右子树
+     */
+    @Test
+    public void middleBinaryTreeTraversal () {
+        TreeNode root = BuildTree.getTree(10);
+        middleTraversal(root);
+    }
+
+    private void middleTraversal(TreeNode node) {
+        if (node.getLeft() != null) {
+            middleTraversal(node.getLeft());
         }
-        System.out.println(nodes.size());
-        return root;
+        System.out.println(node.getData());
+        if (node.getRight() != null) {
+            middleTraversal(node.getRight());
+        }
+    }
+
+    /**
+     * 右序遍历(后序遍历)
+     *
+     * 左-->右-->中
+     */
+    @Test
+    public void rightBinaryTreeTraversal () {
+        TreeNode root = BuildTree.getTree(10);
+        rightTraversal(root);
+    }
+
+    private void rightTraversal(TreeNode node) {
+        if (node.getLeft() != null) {
+            rightTraversal(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            rightTraversal(node.getRight());
+        }
+        System.out.println(node.getData());
     }
 }
